81ea9c1161d811ee899ae5cbff3e280246cd17ea
Kiel Labian <kiellabian@gmail.com>
2016-06-16 21:17:12 +0800

    few fixes

--- market/name/views.py
+++ market/name/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 from .models import BiddingEvent, Name, Bid, Balance
